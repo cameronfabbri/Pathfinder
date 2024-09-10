d718b4eceadb72f82a19d0eb3173f4abf04cdaa0
Cameron Fabbri <cameronfabbri@gmail.com>
2024-09-10 09:55:16 -0600

    added a reverse option

--- scripts/download_websites.py
+++ scripts/download_websites.py
@@ -8,7 +8,7 @@ import subprocess
 
 base_command = 'wget --mirror --convert-links --adjust-extension --page-requisites --no-parent'
 
-def download_websites(json_file):
+def download_websites(json_file, reverse):
     # Read the JSON file
     with open(json_file, 'r') as f:
         data = json.load(f)
@@ -16,7 +16,10 @@ def download_websites(json_file):
     # Create the base directory if it doesn't exist
     base_dir = 'system_data/suny_schools/'
     os.makedirs(base_dir, exist_ok=True)
-    
+
+    if reverse:
+        data = list(reversed(data))
+
     # Process each entry in the JSON file
     for entry in data:
         campus = entry['campus'].replace(' ', '-').replace('_', '').title()
@@ -35,7 +38,10 @@ def download_websites(json_file):
         subprocess.run(command, shell=True)
         print(f"Finished downloading {campus} website.")
 
-
+import sys
 if __name__ == "__main__":
+    reverse = False
     json_file = 'system_data/suny_general/suny_schools.json'
-    download_websites(json_file)
+    if sys.argv[1] == 'reverse':
+        reverse = True
+    download_websites(json_file, reverse)
