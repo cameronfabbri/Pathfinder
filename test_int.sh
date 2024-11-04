f9146ccb889015468ec4a16ce13b79cb49c11939
Ben Zimmer <ben.d.zimmer@gmail.com>
2024-11-04 12:02:06 -0600

    update

--- test_int.sh
+++ test_int.sh
@@ -2,6 +2,6 @@
 
 # Run unit tests.
 
-pytest -s --tb=short --cov-report html:htmlcov/tests --cov=src --cov=tests_int tests_int
+pytest -s --tb=short --cov-report html:htmlcov/int --cov=src --cov=tests_int tests_int
 
-open htmlcov/tests/index.html
+open htmlcov/int/index.html
